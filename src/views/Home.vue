33ddb9652bb237f34b3397296a8c53fed0606b83
irie-shinnosuke <irie-shinnosuke@kayac.com>
2018-09-26 02:03:23 +0900

    一旦最低限はできたんじゃなかろうか

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+    - path = "@/assets/images/"
+    .home
+        input(type="file" ref="allTogether" @change="addFile")
+        input(type="file" ref="file" @change="insertFile")
+        .image-wrapper(ref="imageWrapper")
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import _ from "lodash";
+
+const URL = "https://kiyoshidainagon-node-red-app.mybluemix.net/vr/recognize";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      left: 0,
+      top: 0
+    };
+  },
+  computed: {
+    length() {
+      return _.max([this.height, this.width]);
+    },
+    center() {
+      return {
+        x: this.left + this.width / 2,
+        y: this.top + this.height / 2
+      };
+    },
+    position() {
+      return `circle(${this.length / 2}px at ${this.center.x}px ${
+        this.center.y
+      }px)`;
+    }
+  },
+  methods: {
+    loadFile(file) {
+      return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(file);
+      });
+    },
+    loadImage(data) {
+      return new Promise(resolve => {
+        const image = new Image();
+        image.onload = () => {
+          resolve(image);
+        };
+        image.src = data;
+      });
+    },
+    addFile: async function(evt) {
+      const files = evt.target.files;
+      const data = await this.loadFile(files[0]);
+      const $image = await this.loadImage(data);
+      this.$refs.imageWrapper.appendChild($image);
+    },
+    insertFile: async function(evt) {
+      const files = evt.target.files;
+      const data = await this.loadFile(files[0]);
+      const $image = await this.loadImage(data);
+
+      const base64 = data.replace(/^.*,/, "");
+      const response = await fetch(URL, { method: "POST", body: base64 });
+
+      const json = await response.json();
+      this.width = json.face_location.width;
+      this.height = json.face_location.height;
+      this.left = json.face_location.left;
+      this.top = json.face_location.top;
+
+      $image.style.clipPath = this.position;
+      this.$refs.imageWrapper.appendChild($image);
+    }
   }
 };
 </script>
+
+<style lang="sass">
+    .image-wrapper
+        position: relative
+        width: 800px
+        text-align: left
+</style>
